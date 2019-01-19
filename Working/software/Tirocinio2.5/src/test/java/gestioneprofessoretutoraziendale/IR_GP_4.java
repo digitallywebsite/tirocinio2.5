d4fb96776db3ba5061b8704660efe6da57a6c464
Antonio <antoniosaporito96@gmail.com>
2019-01-19 17:05:44 +0100

    Create IR_GP_4.java

---
+++ Working/software/Tirocinio2.5/src/test/java/gestioneprofessoretutoraziendale/IR_GP_4.java
@@ -0,0 +1,18 @@
+package gestioneprofessoretutoraziendale;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+
+public class IR_GP_4 extends TestCase {
+
+  @Test
+  public void testIR_GP_4_2() {
+
+    String stato = "Acce4554";
+
+    assertEquals("Acce4554", stato);
+  }
+
+}
